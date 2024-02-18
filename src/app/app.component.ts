e7c9778613daff29f4decb6ee5a05fe6c972db3e
Henning Entz <entz.henning@gmail.com>
2024-02-18 20:28:29 +0100

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PersonService} from "./services/person.service";
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule, NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    NgFor,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'damago-datenbank-angular';
+  data: any[] = [];
+
+  constructor(private personService: PersonService) {
+  }
+
+  ngOnInit(): void {
+    this.personService.getPersons().subscribe(result => this.data = result);
+  }
 }
